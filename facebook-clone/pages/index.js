8a3586fe438c297fd6f54de820a1f9f69e2d00c2
maicol <maykolandres4@gmail.com>
2022-11-15 13:34:58 -0500

    created new project

---
+++ facebook-clone/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Facebook</title>
+      </Head>
+
+       <h1>let's build facebook</h1>
+    </div>
+  )
+}

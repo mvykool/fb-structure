2736e674f020ffdf2d5b9a91b299123e65e47309
maicol <maykolandres4@gmail.com>
2022-11-24 06:30:39 -0500

    done sidebar

--- facebook-clone/components/SideBar.jsx
+++ facebook-clone/components/SideBar.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useSession } from 'next-auth/react'
+import { 
+    ChevronDownIcon,
+    ShoppingBagIcon,
+    UserGroupIcon,
+} from '@heroicons/react/outline'
+import { 
+    CalendarIcon,
+    ClockIcon,
+    DesktopComputerIcon,
+    UsersIcon,
+} from '@heroicons/react/solid'
+import SideBarIcon from './SideBarIcon'
 
 const SideBar = () => {
+  
+    const { data: session, loading } = useSession();
+
   return (
-    <div>SideBar</div>
+    <div className='p-2 mt-5 max-w-[600px] xl:min-w-[300px]'>
+        <SideBarIcon src={session.user.image} title={session.user.name}/>
+        
+        <SideBarIcon Icon={UsersIcon} title="Friends" />
+        <SideBarIcon Icon={UserGroupIcon} title="Groups" />
+        <SideBarIcon Icon={ShoppingBagIcon} title="Marketplace" />
+        <SideBarIcon Icon={DesktopComputerIcon} title="watch" />
+        <SideBarIcon Icon={CalendarIcon} title="Events" />
+        <SideBarIcon Icon={ClockIcon} title="Memories" />
+        <SideBarIcon Icon={ChevronDownIcon} title="See More" />
+    </div>
   )
 }
 
--- facebook-clone/pages/index.js
+++ facebook-clone/pages/index.js
@@ -7,7 +7,7 @@ import Login from '../components/Login';
 export default function Home({ session }) {
   if(!session) return <Login/>;
   return (
-    <div >
+    <div className='h-screen bg-gray-100 overflow-hidden'>
       <Head>
         <title>Facebook</title>
       </Head>

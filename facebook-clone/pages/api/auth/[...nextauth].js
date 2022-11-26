5d0718aa4ef252d8d8448b94b5ebbb469d6cbb73
maicol <maykolandres4@gmail.com>
2022-11-26 10:57:48 -0500

    Revert "npm run build"
    This reverts commit 79b3b4c29060bf2ac6a977ef970bc62c088f5b7e.

--- facebook-clone/pages/api/auth/[...nextauth].js
+++ facebook-clone/pages/api/auth/[...nextauth].js
@@ -5,11 +5,10 @@ export default NextAuth({
 
     providers:[
         FacebookProvider({
-            clientId: "662083005462678",
-            clientSecret: "79de7ea82c2886406eb1f0ce2948ed78",
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
           })
     ],
-    secret: "LlKq6ZtYbr+hTC073mAmAh9/h2HwMfsFo4hrfCx6gts=",
 });
 
 
